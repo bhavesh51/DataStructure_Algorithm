0ac3b9a4c87eca6c6cbad0cd4c4796b394841d42
Bhavesh Sadhu <bsadhu51@gmail.com>
2021-08-21 15:05:35 +0530

    array operation

---
+++ array_opr.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arry_opr()
+{
+    cout<<"call from arry_size";
+}
+
+int main()
+{
+    int arr_size;
+    cout<<"enter size of an array:"<<endl;
+    cin>>arr_size;
+    int arr[arr_size];
+    cout<<"enter elements in array:"<<endl;
+    for (int i = 0; i < arr_size; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"your array elements:"<<endl;
+    for (int j = 0; j < arr_size; j++)
+    {
+        cout<<arr[j]<<endl;
+    }
+    
+
+    
+    //arry_opr();
+    return 0;
+}
---
+++ two_dim_opr.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"ops of two dim arr:"<<endl;
+    return 0;
+}
