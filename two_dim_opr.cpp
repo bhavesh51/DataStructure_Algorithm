f68ada424656e6e78c9efd64e3e359c2a77e32d1
Bhavesh Sadhu <bsadhu51@gmail.com>
2021-08-27 10:40:06 +0530

    2D array

--- two_dim_opr.cpp
+++ two_dim_opr.cpp
@@ -1,35 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-class sol
+//const int m = 3;
+class two_dim_arr_opr
 {
     public:
-    int dosum()
+    template <size_t size_x, size_t size_y>
+    void func(double (&arr)[size_x][size_y])
     {
-        cout<<"dosum"<<endl;
-        int a = 6;
-        return a;
+        printf("%p\n", &arr);
     }
 };
 
-
 int main()
 {
-    cout<<"ops of two dim arr:"<<endl;
-    
+    double a1[10][10];
+    double a2[5][5];
+    two_dim_arr_opr obj;
+    printf("%p\n%p\n\n", &a1, &a2);
+    obj.func(a1);
+    obj.func(a2);
 
-    /* 
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-        0 0 0 0 0 0
-    
-    */
-   
-    sol obj1;
-    obj1.dosum();
     return 0;
-
 }
