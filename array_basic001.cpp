98048bb1bdd5e2fca6623a4e312b53980c6b2d08
bhavesh <bsadhu51@gmail.com>
2021-09-05 04:44:34 +0530

    add files

---
+++ array_basic001.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int abc[10];
+    abc[0] = 5;
+    cout<<"bhavesh";
+
+    // char array
+    char ch_arr[] = {'a','e','o'};
+    cout<<ch_arr[1];
+
+    return 0;
+}
