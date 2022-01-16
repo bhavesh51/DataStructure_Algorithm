9dceb82cc393450a5fb55352d1d06bcc8e239319
bhavesh sadhu <bsadhu51@gmail.com>
2022-01-16 12:38:05 +0530

    add basic oop file

---
+++ CPP/CPP_OOP/Basic_OOP.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Basic_OOP
+{
+private:
+    /* data */
+public:
+    Basic_OOP(/* args */);
+    ~Basic_OOP();
+};
+
+Basic_OOP::Basic_OOP(/* args */)
+{
+}
+
+Basic_OOP::~Basic_OOP()
+{
+}
+
+
+int main(int argc, const char** argv) {
+    return 0;
+}
